d078ea0e708431ed2cca0dc65f237062f824f73c
Yadin Yafe <yadin258@yahoo.com>
2022-07-08 19:57:10 +0300

    Start add new editable component

--- components/Task.tsx
+++ components/Task.tsx
@@ -9,10 +9,16 @@ export interface NewTaskProps {
 
 export default function Task(props: NewTaskProps) {
 
+
+
     return (
-        <button className={tw`flex items-center bg-green-500 text-white hover:bg-green-900 p-2 rounded text-sm w-auto`}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm7 14h-5v5h-4v-5h-5v-4h5v-5h4v5h5v4z" /></svg>
-            <span></span>
-        </button>
+        <div className={tw`max-w-[14rem] rounded overflow-hidden shadow-lg`}>
+            <div className={tw`px-6 py-4`}>
+                <div className={tw`font-bold text-lg mb-2`}>{props.title}</div>
+                <p className={tw`text-gray-700 text-sm`}>
+                    {props.content}
+                </p>
+            </div>
+        </div>
     );
 }
--- routes/trello/index.tsx
+++ routes/trello/index.tsx
@@ -2,12 +2,14 @@
 import { h } from "preact";
 import { tw } from "@twind";
 import NewBoardBtn from "../../components/NewBoardBtn.tsx";
+import Task from "../../components/Task.tsx";
 
 export default function Trello() {
   return (
     <div class={tw`p-4 mx-auto max-w-screen-md`}>
       <p>Start you tasks journy by adding new board</p>
       <NewBoardBtn text="Add new board" />
+      <Task title={"New Task title"} content={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."} />
     </div>
   );
 }

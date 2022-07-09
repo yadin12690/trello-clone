1b5f230e5a015f015907611dd7bae191cfd3b744
Yadin Yafe <yadin258@yahoo.com>
2022-07-09 11:35:21 +0300

    yaml

--- components/Editable.tsx
+++ components/Editable.tsx
@@ -1,12 +1,15 @@
-import { Component, h } from 'preact';
-import { useState } from 'preact/hooks';
+/** @jsx h */
+import { tw } from "@twind";
+import { ComponentChildren, h } from 'preact';
+import { useEffect, useState } from 'preact/hooks';
 
 export interface EditableProps {
     text: string;
     type: string;
     placeholder: string,
-    children: Component,
-    props: unknown
+    children: ComponentChildren,
+    props?: unknown,
+    childRef?: any,
 }
 
 // Component accept text, placeholder values and also pass what type of Input - input, textarea so that we can use it for styling accordingly
@@ -14,10 +17,23 @@ export default function Editable(props: EditableProps) {
     // Manage the state whether to show the label or the input box. By default, label will be shown.
     // Exercise: It can be made dynamic by accepting initial state as props outside the component 
     const [isEditing, setEditing] = useState(false);
+    useEffect(() => {
+        if (props.childRef && props.childRef.current && isEditing === true) {
+            props.childRef.current.focus();
+        }
+    }, [isEditing, props.childRef]);
 
-    // Event handler while pressing any key while editing
-    const handleKeyDown = (event: Event, type: unknown) => {
-        // Handle when key is pressed
+    const handleKeyDown = (event: KeyboardEvent, type: string) => {
+        const { key } = event;
+        const keys = ["Escape", "Tab"];
+        const enterKey = "Enter";
+        const allKeys = [...keys, enterKey];
+        if (
+            (type === "textarea" && keys.indexOf(key) > -1) ||
+            (type !== "textarea" && allKeys.indexOf(key) > -1)
+        ) {
+            setEditing(false);
+        }
     };
 
     /*
@@ -27,19 +43,20 @@ export default function Editable(props: EditableProps) {
     Note: For simplicity purpose, I removed all the classnames, you can check the repo for CSS styles
     */
     return (
-        <section {...props.props}>
+        <section {...props}>
             {isEditing ? (
                 <div
                     onBlur={() => setEditing(false)}
-                    onKeyDown={e => handleKeyDown(e, type)}
+                    onKeyDown={e => handleKeyDown(e, props.type)}
                 >
                     {props.children}
                 </div>
             ) : (
                 <div
+                    className={tw`rounded py-2 px-3 text-gray-700 leading-tight whitespace-pre-wrap hover:shadow-outline editable-${props.type}`}
                     onClick={() => setEditing(true)}
                 >
-                    <span>
+                    <span className={`${props.text ? "text-black" : "text-gray-500"}`}>
                         {props.text || props.placeholder || "Editable content"}
                     </span>
                 </div>
--- components/Task.tsx
+++ components/Task.tsx
@@ -1,6 +1,8 @@
 /** @jsx h */
-import { h } from "preact";
 import { tw } from "@twind";
+import { h } from "preact";
+import { useState } from 'preact/hooks';
+import Editable from "./Editable.tsx";
 
 export interface NewTaskProps {
     title: string;
@@ -8,13 +10,27 @@ export interface NewTaskProps {
 }
 
 export default function Task(props: NewTaskProps) {
-
+    const [task, setTask] = useState("");
 
 
     return (
         <div className={tw`max-w-[14rem] rounded overflow-hidden shadow-lg`}>
             <div className={tw`px-6 py-4`}>
-                <div className={tw`font-bold text-lg mb-2`}>{props.title}</div>
+                <div className={tw`font-bold text-lg mb-2`}>
+                    <Editable
+                        text={task}
+                        placeholder="Write a task name"
+                        type="input"
+                    >
+                        <input
+                            type="text"
+                            name="task"
+                            placeholder="Write a task name"
+                            value={task}
+                            onChange={e => setTask(e.target?.value)}
+                        />
+                    </Editable>
+                </div>
                 <p className={tw`text-gray-700 text-sm`}>
                     {props.content}
                 </p>
